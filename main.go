e8b636fc75725871bb6f4b0c9dbd56b255b1c1e7
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-12-07 11:22:22 +0100

    todos endpoint get

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+)
 
 type todo struct {
-	ID        uuid.UUID
-	Item      string
-	Completed bool
+	ID        uuid.UUID `json:"id"`
+	Item      string    `json:"item"`
+	Completed bool      `json:"completed"`
+}
+
+var todos = []todo{
+	{
+		ID:        uuid.New(),
+		Item:      "Clean Room",
+		Completed: false,
+	},
+	{
+		ID:        uuid.New(),
+		Item:      "Cooking",
+		Completed: false,
+	},
+	{
+		ID:        uuid.New(),
+		Item:      "Watch movies",
+		Completed: true,
+	},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/todos", getTodos)
+	router.Run("localhost:12345")
 }
